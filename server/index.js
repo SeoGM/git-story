a207ffdfe2bbe615a382a1d09cb2a15b9263bf56
GwangMyeong Seo <auddlzz000@gmail.com>
2024-10-13 15:04:37 +0000

    feat: react와 express 연동 예제

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
+// server.js
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.get("/api", (req, res) => {
-  res.send("Hello from Express");
+const app = express();
+const PORT = 4000;
+
+app.use(
+  cors({
+    origin: "https://3000-seogm-gitstory-1ayu6plkg6n.ws-us116.gitpod.io",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Hello from the server!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
